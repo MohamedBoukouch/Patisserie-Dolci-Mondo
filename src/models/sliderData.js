a8dcd5a4e458a9b44d3512162a80b9d273bcf2e7
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-02-07 19:13:47 +0100

    add routing vercel

--- src/models/sliderData.js
+++ src/models/sliderData.js
@@ -2,19 +2,19 @@ export const slides = [
     {
       id: 1,
       type: "video",
-      src: "https://res.cloudinary.com/ds9v1rpfi/video/upload/v1770477307/medium-vecteezy_chef-lady-putting-cream-on-top-of-cake-while-making-homemade_8018930_medium_plaa9b.mp4",
-      text: "مرحبا بكم في دولسي موندو"
+      src: "../../public/vids/vid1.mp4",
+      text: " DOLCI MONDO مرحبا بكم في "
     },
     {
       id: 2,
       type: "video",
-      src: "https://res.cloudinary.com/ds9v1rpfi/video/upload/v1770476548/vecteezy_croissant-wrapped-hotdog-pastries-in-display-case_13286100_daoivh.mp4",
+      src: "../../public/vids/vid2.mp4",
       text: "شاهد حلوياتنا تتحضر"
     },
     {
       id: 3,
       type: "video",
-      src: "https://res.cloudinary.com/ds9v1rpfi/video/upload/v1770476927/x-large-vecteezy_slice-of-strawberry-chocolate-cake-on-a-plate_47364355_x-large_dn8mkm.mp4",
+      src: "../../public/vids/vid3.mp4",
       text: "حلويات تقليدية وعصرية"
     }
   ];
