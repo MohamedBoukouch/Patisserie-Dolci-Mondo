6e2767c3c73b442ea684e386de41c700fcb787bd
mac <mac@MacBook-Pro-de-mac.local>
2026-03-11 15:01:51 -0700

    add reel products

--- src/models/products.js
+++ src/models/products.js
@@ -3,70 +3,63 @@ export const products = [
     id: 1,
     nameKey: "products.macaron_laduree",
     price: 15 ,
-    img: "/images/products/prd_1.webp",
-    categoryKey: "categories.macaron"
-  },
-  {
-    id: 2,
-    nameKey: "products.tote_bag",
-    price: 12,
-    img: "/images/products/prd_12.webp",
-    categoryKey: "categories.gifts"
-  },
-  {
-    id: 3,
-    nameKey: "products.heart_box",
-    price: 25,
-    img: "/images/products/prd_11.webp",
-    categoryKey: "categories.new"
-  },
-  {
-    id: 4,
-    nameKey: "products.mug_rose",
-    price: 15,
-    img: "/images/products/prd_10.webp",
+    img: "/images/products/prd_1.jpg",
     categoryKey: "categories.macaron"
   },
   {
     id: 5,
     nameKey: "products.heart_box",
     price: 25,
-    img: "/images/products/prd_9.webp",
+    img: "/images/products/prd_2.jpg",
     categoryKey: "categories.new"
   },
   {
     id: 6,
     nameKey: "products.mug_rose",
     price: 15,
-    img: "/images/products/prd_8.webp",
+    img: "/images/products/prd_3.jpg",
     categoryKey: "categories.macaron"
   },
   {
     id: 7,
     nameKey: "products.heart_box",
     price: 25,
-    img: "/images/products/prd_7.webp",
+    img: "/images/products/prd_4.jpg",
     categoryKey: "categories.new"
   },
   {
     id: 8,
     nameKey: "products.mug_rose",
     price: 15,
-    img: "/images/products/prd_9.webp",
+    img: "/images/products/prd_5.jpg",
     categoryKey: "categories.macaron"
   },
   {
     id: 9,
     nameKey: "products.mug_rose",
     price: 15,
-    img: "/images/products/prd_4.avif",
+    img: "/images/products/prd_6.jpg",
     categoryKey: "categories.macaron"
   },
   {
     id: 10,
     nameKey: "products.mug_rose",
     price: 15,
-    img: "/images/products/prd_3.avif",
+    img: "/images/products/prd_7.jpg",
+    categoryKey: "categories.macaron"
+  },
+    {
+    id: 9,
+    nameKey: "products.mug_rose",
+    price: 15,
+    img: "/images/products/prd_8.jpg",
+    categoryKey: "categories.macaron"
+  },
+  {
+    id: 10,
+    nameKey: "products.mug_rose",
+    price: 15,
+    img: "/images/products/prd_9.jpg",
     categoryKey: "categories.macaron"
   }
 ];
