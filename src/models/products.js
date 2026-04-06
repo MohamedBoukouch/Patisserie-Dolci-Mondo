1d70b545dfea82854462514fcb7062331e245ebe
mac <mac@192.168.11.198>
2026-04-06 16:43:30 +0100

    add some design updates

--- src/models/products.js
+++ src/models/products.js
@@ -4,55 +4,55 @@ export const products = [
     nameKey: "products.macaron_laduree",
     price: 15,
     img: "/images/products/prd_1.jpg",
-    categoryKey: "categories.macaron"
+    categoryKey: "categories.arabic_sweets"
   },
   {
     id: 2,
     nameKey: "products.heart_box",
     price: 25,
     img: "/images/products/prd_2.jpg",
-    categoryKey: "categories.new"
+    categoryKey: "categories.arabic_sweets"
   },
   {
     id: 3,
     nameKey: "products.heart_box",
     price: 25,
     img: "/images/products/prd_4.jpg",
-    categoryKey: "categories.new"
+    categoryKey: "categories.Box_of_the_gathering"
   },
   {
     id: 4,
     nameKey: "products.mug_rose",
     price: 15,
     img: "/images/products/prd_5.jpg",
-    categoryKey: "categories.macaron"
+    categoryKey: "categories.Box_of_the_gathering"
   },
   {
     id: 5,
     nameKey: "products.mug_rose",
     price: 15,
     img: "/images/products/prd_6.jpg",
-    categoryKey: "categories.wedding"
+    categoryKey: "categories.chocolate_box"
   },
   {
     id: 6,
     nameKey: "products.mug_rose",
     price: 15,
     img: "/images/products/prd_7.jpg",
-    categoryKey: "categories.wedding"
+    categoryKey: "categories.chocolate_box"
   },
   {
     id: 7,
     nameKey: "products.mug_rose",
     price: 15,
     img: "/images/products/prd_8.jpg",
-    categoryKey: "categories.birthday"
+    categoryKey: "categories.chocolate_box"
   },
   {
     id: 8,
     nameKey: "products.mug_rose",
     price: 15,
     img: "/images/products/prd_9.jpg",
-    categoryKey: "categories.engagement"
+    categoryKey: "categories.chocolate_box"
   }
 ];
